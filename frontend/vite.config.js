1a6ba7609fc9db590e232f7887dc2c681772df6d
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-08-07 14:57:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      'https://to-do-list-wf6p.onrender.com/api': {
         target: 'https://to-do-list-backend-mojb.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
