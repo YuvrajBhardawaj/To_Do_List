9864e6cc37518dcc0f2770e2511e679aaeb95dd4
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-08-07 14:48:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{'/api':'https://to-do-list-backend-mojb.onrender.com/'},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://to-do-list-backend-mojb.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   plugins: [react()],
-})
+});
